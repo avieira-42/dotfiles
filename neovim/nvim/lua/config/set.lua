dab56b6421876992607ee74a28d4023a21ea3003
avieira-42 <vieiramendesalex@gmail.com>
2025-10-20 18:49:26 +0100

    created token_identify.c | need to add identification of the first token

--- neovim/nvim/lua/config/set.lua
+++ neovim/nvim/lua/config/set.lua
@@ -6,32 +6,46 @@
 --   By: avieira- <avieira-@student.42porto.com>    +#+  +:+       +#+        --
 --                                                +#+#+#+#+#+   +#+           --
 --   Created: 2025/09/24 20:51:45 by avieira-          #+#    #+#             --
---   Updated: 2025/09/25 18:36:19 by avieira-         ###   ########.fr       --
+--   Updated: 2025/10/20 18:40:50 by avieira-         ###   ########.fr       --
 --                                                                            --
 -- ************************************************************************** --
 
+
+-- Line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+-- Tabs and indentation
+vim.opt.tabstop = 4          -- Show tabs as 4 spaces wide
+vim.opt.softtabstop = 4      -- Editing operations treat tab as 4 spaces
+vim.opt.shiftwidth = 4       -- Indent levels are 4 spaces
+vim.opt.expandtab = false    -- NEVER convert tabs to spaces
+vim.opt.smartindent = true   -- Smart autoindenting
 
-vim.opt.smartindent = true
+-- Enforce tabs for all filetypes
+vim.cmd([[
+  set noexpandtab
+  filetype plugin indent on
+  autocmd FileType * setlocal noexpandtab
+]])
 
+-- Text wrapping
 vim.opt.wrap = false
 
+-- Swap/backup/undo
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- Search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- Clipboard
 vim.opt.clipboard = "unnamedplus"
 
+-- UI tweaks
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
