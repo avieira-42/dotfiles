470c57b68f8525f7e7a770637b107383104fcefe
avieira-42 <vieiramendesalex@gmail.com>
2025-10-25 17:32:41 +0100

    removed kickstart

--- neovim/nvim/nvim/lua/config/remap.lua
+++
@@ -1,8 +0,0 @@
--- Leader key
-vim.g.mapleader = " "
-
--- normal mode
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex) -- remap Ex to be leader<pv>
-
--- insert mode
-vim.keymap.set('i', 'jj', '<Esc>') -- remap esc to be jj
--- neovim/nvim/nvim/lua/plugins/undotree.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "mbbill/undotree",
-    config = function()
-      -- Keymap to toggle undo tree
-      vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle UndoTree" })
-    end,
-  },
-}
